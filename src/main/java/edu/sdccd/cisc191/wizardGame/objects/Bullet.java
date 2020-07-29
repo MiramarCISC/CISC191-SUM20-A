18e6f892f1835029fb48a8156a4f555adf79c4e9
Mark Lucernas <marklcrns@gmail.com>
2020-07-29 00:15:47 -0700

    Update BulletTest and Bullet Javadocs

--- src/main/java/edu/sdccd/cisc191/wizardGame/objects/Bullet.java
+++ src/main/java/edu/sdccd/cisc191/wizardGame/objects/Bullet.java
@@ -6,10 +6,35 @@ import java.awt.Rectangle;
 
 import edu.sdccd.cisc191.wizardGame.utils.images.SpriteSheet;
 
+/**
+ * Bullet game object.
+ * Bullet velocity depends on how far away the mouse click from the
+ * {@code Wizard} instance.
+ *
+ * @author Jordan Tobin
+ *
+ * Editor: Mark Lucernas
+ * Date: 2020-06-08
+ */
 public class Bullet extends GameObject {
 
     private Handler handler;
 
+    /**
+     * No args Bullet constructor.
+     */
+    public Bullet() { super(); }
+
+    /**
+     * Bullet constructor.
+     * @param x         X coordinate
+     * @param y         Y coordinate
+     * @param id        Object ID
+     * @param handler   Handler to handle all GameObjects
+     * @param mx        Bullet X speed
+     * @param my        Bullet Y speed
+     * @param ss        Main Spritesheet
+     */
     public Bullet(int x, int y, ID id, Handler handler, int mx, int my, SpriteSheet ss) {
         super(x, y, id, ss);
         this.handler = handler;
@@ -18,28 +43,41 @@ public class Bullet extends GameObject {
         velY = (my - y) / 10;
     }
 
+    /**
+     * Tick method to remove Bullet object from handler on collision.
+     */
     public void tick() {
         x += velX;
         y += velY;
 
         // If bullet intersects with a block, remove said bullet.
-        for(int i = 0; i < handler.object.size(); i++) {
-            GameObject tempObject = handler.object.get(i);
+        for(int i = 0; i < handler.getObject().size(); i++) {
+            GameObject tempObject = handler.getObject().get(i);
 
-            if (tempObject.getId() == ID.Block) {
-                if (getBounds().intersects(tempObject.getBounds())) {
+            if (tempObject.getId() == ID.Block)
+                if (getBounds().intersects(tempObject.getBounds()))
                     handler.removeObject(this);
-                }
-            }
         }
     }
 
+    /**
+     * Render method to draw in {@code Canvas}.
+     */
     public void render(Graphics g) {
         g.setColor(Color.magenta);
         g.fillOval(x, y, 8, 8);
     }
 
+    /**
+     *  Get Bullet current bounds.
+     */
     public Rectangle getBounds() {
         return new Rectangle(x, y, 8, 8);
     }
+
+    /** Accessor methods */
+    public Handler getHandler()             { return this.handler; }
+
+    /** Modifier methods */
+    public void setHandler(Handler handler) { this.handler = handler; }
 }
--- src/test/java/edu/sdccd/cisc191/wizardGame/objects/BulletTest.java
+++ src/test/java/edu/sdccd/cisc191/wizardGame/objects/BulletTest.java
@@ -3,12 +3,11 @@ package edu.sdccd.cisc191.wizardGame.objects;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.awt.Rectangle;
-import java.awt.image.BufferedImage;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import edu.sdccd.cisc191.wizardGame.utils.images.BufferedImageLoader;
 import edu.sdccd.cisc191.wizardGame.utils.images.SpriteSheet;
 
 /**
@@ -17,43 +16,76 @@ import edu.sdccd.cisc191.wizardGame.utils.images.SpriteSheet;
  */
 class BulletTest {
 
-  private static Bullet bullet;
-  private static Rectangle rectangle;
+    private Bullet bullet;
+    private Rectangle rectangle;
+    private Handler handler;
+    private static int x;
+    private static int y;
+    private static int mx;
+    private static int my;
+    private static int velX;
+    private static int velY;
 
-  @BeforeAll
-  static void init() {
-    int x = 1;
-    int y = 1;
-    int mx = 5;
-    int my = 5;
-    int velX = (mx - x) / 10;
-    int velY = (my - y) / 10;
-    ID id = ID.Bullet;
-    Handler handler = new Handler();
-    rectangle = new Rectangle(x, y, 8, 8);
+    @BeforeAll
+    static void init() {
+        // Initialize static fields
+        x = 1;
+        y = 1;
+        velX = (mx - x) / 10;
+        velY = (my - y) / 10;
+    }
 
-    BufferedImageLoader loader = new BufferedImageLoader();
-    BufferedImage spriteSheetImage = loader.loadImage("/main_sheet.png");
-    SpriteSheet spriteSheet = new SpriteSheet(spriteSheetImage);
-    bullet = new Bullet(x, y, id, handler, mx, my, spriteSheet);
-  }
+    @BeforeEach
+    void setUp() {
+        handler = new Handler();
 
-  @Test
-  void tickTest() {
-    bullet.tick();
-    // TODO: Figure out
-  }
+        // Set up Bullet
+        bullet = new Bullet();
+        bullet.setX(x);
+        bullet.setY(y);
+        bullet.setVelX(velX);
+        bullet.setVelY(velY);
+        bullet.setId(ID.Bullet);
+        bullet.setHandler(handler);
 
-  @Test
-  void renderTest() {
-    // TODO: Figure out
-  }
+        // Add to handler (the order matters)
+        handler.addObject(bullet);
 
-  @Test
-  void getBoundsTest() {
-    Rectangle expected = rectangle;
-    Rectangle actual = bullet.getBounds();
 
-    assertEquals(expected, actual, "Gets Bullet bounds");
-  }
+        rectangle = new Rectangle(x, y, 8, 8);
+    }
+
+    @Test
+    void testTickBlockCollision() {
+        // Set up Block
+        Block block = new Block();
+        block.setX(x += velX); // Match Bullet x coords after tick()
+        block.setY(y += velY); // Match Bullet y coords after tick()
+        block.setId(ID.Block);
+        handler.addObject(block);
+
+        bullet.tick();
+        GameObject object = handler.getObject().getFirst();
+        assertEquals(1, handler.getObject().size(), "Check if Bullet is removed");
+        assertEquals(ID.Block, object.getId(), "Check if remaining object is Block");
+    }
+
+    @Test
+    void testTickNonBlockCollision() {
+        // Set up Block
+        Minion minion = new Minion();
+        minion.setX(x += velX); // Match Bullet x coords after tick()
+        minion.setY(y += velY); // Match Bullet y coords after tick()
+        minion.setId(ID.Minion);
+        handler.addObject(minion);
+
+        bullet.tick();
+        assertEquals(2, handler.getObject().size(), "Check if Bullet is not removed");
+    }
+
+    @Test
+    void testGetBounds() {
+        Rectangle actual = bullet.getBounds();
+        assertEquals(rectangle, actual, "Gets Bullet Rectangle bounds");
+    }
 }
