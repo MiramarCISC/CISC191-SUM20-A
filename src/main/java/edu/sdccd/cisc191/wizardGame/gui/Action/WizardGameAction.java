e045b405497a7b428d10a12d262a472f18b1c36a
Seth <steenfuentes@gmail.com>
2020-08-05 20:38:05 -0700

    Complete actions package

--- src/main/java/edu/sdccd/cisc191/wizardGame/gui/Action/WizardGameAction.java
+++ src/main/java/edu/sdccd/cisc191/wizardGame/gui/Action/WizardGameAction.java
@@ -1,12 +1,20 @@
 package edu.sdccd.cisc191.wizardGame.gui.Action;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.sdccd.cisc191.wizardGame.Game;
 import edu.sdccd.cisc191.wizardGame.gui.screen.Window;
 import edu.sdccd.cisc191.wizardGame.option.Option;
+
+/**
+ * Super class of actions triggered by buttons within the WizardGame GUI. Subclasses
+ * that inherit {@code WizardGameAction} are stored within a map in {@code ActionManager}.
+ *
+ * @author Seth Steen-Fuentes
+ *
+ * 08/05/2020
+ */
 
 public abstract class WizardGameAction extends AbstractAction implements Option<WizardGameAction>, ActionListener {
 
@@ -24,8 +32,6 @@ public abstract class WizardGameAction extends AbstractAction implements Option<
     protected ActionManager getActionManager() {
         return frame.getActionManager();
     }
-
-
 
     protected Game getGame()                { return frame.getGame(); }
     protected Window getWindow()              { return getGame().getFrame(); }
